5134e2bc9161aa9157837c6faa017f19425228e5
Wooseop Kim <wooseop.kim.dev@gmail.com>
2018-03-30 20:56:41 +0900

    Week 2 finished

--- app/src/main/java/org/appeyroad/s11/introduction/MainActivity.kt
+++ app/src/main/java/org/appeyroad/s11/introduction/MainActivity.kt
@@ -5,7 +5,11 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.appeyroad.s13.arithmetica.Arithmetica
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inputList = mutableListOf<String>()
+
+        val listener = View.OnClickListener { view ->
+            val text = (view as Button).text
+            inputList.add(text.toString())
+        }
+
+        arrayOf(
+                one, two, three,
+                four, five, six,
+                seven, eight, nine,
+                zero, double_zero
+        ).forEach { it.setOnClickListener(listener) }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

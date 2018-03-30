65142675916689c9ba0995ef0c0880fcea433ee4
Wooseop Kim <wooseop.kim.dev@gmail.com>
2018-03-30 19:03:08 +0900

    Catch-up commit for newcomer

--- app/src/main/java/org/appeyroad/s11/introduction/MainActivity.kt
+++ app/src/main/java/org/appeyroad/s11/introduction/MainActivity.kt
@@ -5,12 +5,14 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
